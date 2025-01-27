cb0dbb047838bbce123107d1758e30b152f2d108
Dipu Sah <sahd7929@gmail.com>
2025-01-27 11:38:05 +0530

    comments added

--- src/App.jsx
+++ src/App.jsx
@@ -25,57 +25,52 @@ function App() {
 
   return (
   <div className="bg-gray-500 h-full ">
+
     <div className="p-4 m-4">
-
-    
-
 
     <div className=" p-4 m-4  text-3xl font-bold text-center text-white">
       Loan Calculator
     </div>
 
     <div className=" p-4 m-4 outline outline-offset-2 text-3xl font-bold text-center text-white">
-    Maximum Funding 
-    <h2>Rs 100000 </h2>
+      Maximum Funding 
+      <h2>Rs 100000 </h2>
     </div>
 
-
+    {/* Loan Amount */}
     <div className="p-4 m-4 outline outline-offset-2 text-3xl font-bold text-center text-white">
       <h1 >Loan Amount</h1>
       <h1>{amount} Amounts</h1>
-    <Slider 
-      min={10000} 
-      max={100000} 
-      defaultValue={50} 
-      aria-label="Default" 
-      valueLabelDisplay="auto" 
-      onChange={amountChange}/>
-
-
+      <Slider 
+        min={10000} 
+        max={100000} 
+        defaultValue={50} 
+        aria-label="Default" 
+        valueLabelDisplay="auto" 
+        onChange={amountChange}/>
       </div>
 
+    {/* Loan Period */}
       <div className=" p-4 m-4 outline outline-offset-2 text-3xl font-bold text-center text-white">
         <h1>Loan Period</h1>
         <h1>{months} Months</h1>
-    <Slider 
-      min={1} 
-      max={24} 
-      defaultValue={5} 
-      aria-label="Default" 
-      valueLabelDisplay="auto" 
-      onChange={monthChange}/>
-
-
+        <Slider 
+          min={1} 
+          max={24} 
+          defaultValue={5} 
+          aria-label="Default" 
+          valueLabelDisplay="auto" 
+          onChange={monthChange}/>
       </div>
 
       </div>
 
+      {/* monthly Estimated */}
       <div className=" p-4 m-4  text-3xl font-bold text-center text-white">
-      Estimated monthly installment
-
-      <h1>Rs. {installment}</h1>
-      <button className="bg-blue-500 p-4 rounded-lg w-max">Apply Loan</button>
-    </div>
+        Estimated monthly installment
+        <h1>Rs. {installment}</h1>
+        <button className="bg-blue-500 p-4 rounded-lg w-max">Apply Loan</button>
+      </div>
   </div>
   )
 }
